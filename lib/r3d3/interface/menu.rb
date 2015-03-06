1b71ae81252dad37b27b2b5f059134f19b101977
Alban Diguer <alban.diguer@gmail.com>
2015-03-06 19:11:28 +1100

    Remove thor, handcraft menu.

--- lib/r3d3/interface/menu.rb
+++ lib/r3d3/interface/menu.rb
@@ -1,10 +1,49 @@
-require 'thor'
+require_relative '../models/world'
 
-class Menu < Thor
+class Menu
 
-  desc "place X,Y,Z", "position the robot on the playground"
-  def place(position)
-    puts position
+  def initialize
+    puts "\e[32mWelcome in toy robot simulator!\e[0m"
+    puts %q(
+             _
+            [ ]
+           (   )
+            |>|
+         __/===\__
+        //| o=o |\\
+      <]  | o=o |  [>
+          \=====/
+         / / | \ \
+        <_________>
+    )
+    # instantiate_world
+    # play_now
+    instantiate_world
+    print_commands 
+  end
+
+private 
+
+  def instantiate_world
+    @world = World.new
+  end
+
+# TODO
+  def play_now
+    # make a world
+    # print_commands
+  end
+
+  def print_commands
+    puts 'Use the following commands:'
+    puts %q(
+      PLACE X,Y,F
+      MOVE
+      LEFT
+      RIGHT
+      REPORT
+      EXIT
+    )
   end
 
 end
