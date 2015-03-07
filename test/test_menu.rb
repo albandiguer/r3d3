4ae075a3fef379e1b2c87a115bdb6d732b016a26
Alban Diguer <alban.diguer@gmail.com>
2015-03-08 00:05:23 +1100

    WIP: testMenu

---
+++ test/test_menu.rb
@@ -0,0 +1,13 @@
+require 'minitest_helper'
+require_relative '../lib/r3d3/models/robot'
+
+class TestMenu < MiniTest::Test 
+
+  # XXX
+  # def test_instanciation
+  #   mock = Minitest::Mock.new 
+  #   mock.expect(:instantiate_world, nil)
+  # end
+
+end
+
